2f93d3ce7c5e6f19043d452b25eecd9a8db77be5
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2022-12-20 10:12:07 -0600

    importing react useState and declaring initial form values

--- users/src/App.js
+++ users/src/App.js
@@ -1,5 +1,13 @@
 import Form from "./Components/Form";
+import React, { useState } from "react";
 import './App.css';
+
+const initialFormValues = {
+  username: '',
+  password: '',
+  email: '',
+  checked: false
+}
 
 function App() {
   return (
